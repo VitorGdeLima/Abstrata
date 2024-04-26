264533f9a5b35c324382da3c0f153c332d8e10b0
VitorGdeLima <166819522+VitorGdeLima@users.noreply.github.com>
2024-04-26 08:58:24 -0300

    Projeto Criado
    Criei Projeto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Abstrata;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		ContaPoupanca poupanca = new ContaPoupanca();
+		
+		poupanca.numeroDaConta = 1000;
+		poupanca.titularDaConta = "zeh da manga";
+		poupanca.Deposito(20000.00);
+	}
+}
